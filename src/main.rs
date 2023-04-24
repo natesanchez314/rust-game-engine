9e7de69c8a3dacaba32b4f3ba1425e20211657a8
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2023-04-24 18:34:20 -0500

    render pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,4 @@
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
- 
+use poblano::run;
 fn main() {
-    env_logger::init(); // Necessary for logging within WGPU
-    let event_loop = EventLoop::new(); // Loop provided by winit for handling window events
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    let instance = wgpu::Instance::new(wgpu::Backends::all());
-    let surface = unsafe { instance.create_surface(&window) };
-    let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
-        power_preference: wgpu::PowerPreference::default(),
-        compatible_surface: Some(&surface),
-        force_fallback_adapter: false,
-    })).unwrap();
-
-    let (device, queue) = pollster::block_on(adapter.request_device(
-        &wgpu::DeviceDescriptor {
-            label: None,
-            features: wgpu::Features::empty(),
-            limits: wgpu::Limits::default()
-        },
-        None,
-    )).unwrap();
-
-    let size = window.inner_size();
-    surface.configure(&device, &wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: surface.get_preferred_format(&adapter).unwrap(),
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Fifo,
-    });
- 
-     // Opens the window and starts processing events (although no events are handled yet)
-    event_loop.run(move |event, _, control_flow| {
-        
-    });
+    pollster::block_on(run());
 }
