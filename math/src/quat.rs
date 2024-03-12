97c4611692680501391b3f8705a1c3eef4c5d7fe
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2024-03-12 12:00:37 -0500

    layout for quat

--- math/src/quat.rs
+++ math/src/quat.rs
@@ -1,6 +1,110 @@
+enum Axis {
+    X,
+    Y,
+    Z,
+}
+
+enum Orient {
+    LocalToWorld,
+    WorldToLocal,
+}
+
 pub struct Quat {
     x: f32,
     y: f32,
     z: f32,
     real: f32
 }
+
+impl Quat {
+
+    fn zero() -> Self {
+        Self {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+            real: 0.0,
+        }
+    }
+
+    fn identity() -> Self {
+        Self {
+            x: 0.0, 
+            y: 0.0,
+            z: 0.0,
+            real: 1.0
+        }
+    }
+
+    fn lqcvq() {
+        todo!();
+    }
+
+    fn lqvqc() {
+        todo!();
+    }
+
+    fn conj(&mut self) {
+        self.x = -self.x;
+        self.y = -self.y;
+        self.z = -self.z;
+    }
+
+    fn get_conj(&self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+            real: 1.0,
+        }
+    }
+
+    fn t() {
+
+    }
+
+    fn get_t() {
+
+    }
+
+    fn get_mag() -> f32 {
+        0.0
+    }
+
+    fn get_mag_sqr() -> f32 {
+        0.0
+    }
+
+    fn get_inv_mag() -> f32 {
+        0.0
+    }
+
+    fn norm() {
+        
+    }
+
+    fn get_norm() {
+
+    }
+
+    fn inv() {
+
+    }
+
+    fn get_inv() {
+
+    }
+
+    fn dot() {
+
+    }
+
+    fn get_angle() {
+
+    }
+
+    fn get_axis() {
+        
+    }
+}
+
